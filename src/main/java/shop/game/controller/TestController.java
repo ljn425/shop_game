c656883a4b8ea71f81400bc63437d294d7ae73f5
ljn425 <ljn425@naver.com>
2023-05-04 16:41:11 +0900

    Initial commit

---
+++ src/main/java/shop/game/controller/TestController.java
@@ -0,0 +1,14 @@
+package shop.game.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "main";
+    }
+
+}
